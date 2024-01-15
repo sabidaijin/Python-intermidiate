6707c79f7bea3a6534410f733af9e482e8d1ff83
sabidaijin <yamahb@icloud.com>
2024-01-15 18:20:40 +0900

    最低限のニューラルネットワーク

--- python_mid_NN.py
+++ python_mid_NN.py
@@ -23,40 +23,78 @@
 
 # 順伝播の計算を実装する（バックプロパテーションはスコープアウト)
 # 
+import math
 
-def sigm():
-    1/1+e**-x
 
-def relu():
-    if x>0:
-        x
+def sigmoid(x):
+    """シグモイド関数"""
+    return 1.0/ (1 + math.exp(-x))
+
+def relu(x):
+    """ReLU関数"""
+    return max(0, x)
+
+def forward_propagation(input_data, flag):
+    """順伝播の計算を行う"""
+    W1 = [
+        [-1.01, 1.03, -1.05, 1.07, -1.09],
+        [1.11, -1.13, 1.15, -1.17, 1.19],
+        [-1.21, 1.23, -1.25, 1.27, -1.29]
+    ]
+
+    B1 = [-1.31, -1.33, 1.35, -1.37, 1.39]
+
+    W2 = [
+        [2.02, 2.04, -2.06, 2.08],
+        [-2.1, 2.12, -2.14, 2.16],
+        [-2.18, 2.2, -2.22, 2.24],
+        [-2.26, 2.28, -2.3, 2.32],
+        [-2.34, 2.36, -2.38, 2.4]
+    ]
+
+    B2 = [-2.42, 2.44, -2.46, 2.48]
+
+
+    if flag == 0:
+        activation_function = sigmoid
+    elif flag == 1:
+        activation_function = relu
     else:
-        0
-def sim():
+        activation_function = lambda x: x
+
+    # 圧縮されて配列になる
+    layer1_output = []
+    # 第一層の出力を計算
+    # iが横の列、jが縦の列
+    
+    for i in range(len(W1[0])):
+        # layer1=x*W1+B1
+        weighted_sum = 0
+        for j in range(len(input_data)):
+            #行列の掛け算は、転置を使う
+            weighted_sum += input_data[j] * W1[j][i] 
+        layer1_output.append(activation_function(weighted_sum+ B1[i]))
 
 
-W1=[
-[-1.01,  1.03, -1.05,  1.07, -1.09]
-[1.11, -1.13,  1.15, -1.17,  1.19]
-[-1.21,  1.23, -1.25,  1.27, -1.29]]
+    layer2_output = []
+    # 第二層の出力を計算
+    for i in range(len(W2[0])):
+        # layer2=layer1_output*W2+B2
+        weighted_sum = 0
+        
+        for j in range(len(layer1_output)):
+            #行列の掛け算は、転置を使う
+            weighted_sum += layer1_output[j] * W2[j][i] 
+        layer2_output.append(activation_function(weighted_sum+ B2[i]))
 
-B1=[-1.31,-1.33,1.35,-1.37,1.39]
-
-W2=[
-    [2.02, 2.04, -2.06, 2.08]
-    [-2.1 , 2.12, -2.14, 2.16]
-    [-2.18, 2.2 , -2.22, 2.24]
-    [-2.26, 2.28, -2.3 , 2.32]
-    [-2.34, 2.36, -2.38, 2.4 ]]
-
+    return layer2_output
 
 
-B2=[-2.42,2.44,-2.46,2.48]
-n=int(input())
 
-if flag==0:
-    sigm()
-else if flag==1:
-    relu()
-else:
-    sim()
+# 入力データ: (2, 1, 3)
+input_data = [2, 1, 3]
+
+# フラグ 0 シグモイド関数 1 ReLU関数 2 活性化関数無し
+flag = 0
+output = forward_propagation(input_data, flag)
+print("出力:", output)
